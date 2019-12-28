0b850985ac3bcd5a94080a4b00e16730d85c6c3e
re-volted <bartoszkow94@gmail.com>
2019-12-28 14:05:54 +0100

    Router file cleanup.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,23 +4,18 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [{
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   component: () =>
-  //     import( /* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: Home
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
